427f8a1c20db97a843f7adf9228a51d73e5597da
Willy <cromir@mail.ch>
2021-10-23 00:27:21 +0200

    refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,110 +1,18 @@
 import React from 'react';
-import {
-    alpha,
-    AppBar,
-    Box,
-    Button,
-    Card,
-    CardContent,
-    Container,
-    InputBase,
-    MenuItem,
-    Select,
-    Slider,
-    styled,
-    Toolbar,
-    Typography
-} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import {useThemeSettings} from "./config/theme";
+import {Box, Button, Card, CardContent, Container, Toolbar} from "@mui/material";
+import {AppBar} from "./components/AppBar";
+import {FontSwitcher} from "./components/FontSwitcher";
 
-
-const Search = styled('div')(({theme}) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({theme}) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({theme}) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
-}));
 
 function App() {
-    const {fontSize, setFontSize, font, setFont} = useThemeSettings()
-
     return (
         <Container>
-            <Box sx={{flexGrow: 1}}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{display: {xs: 'none', sm: 'block'}}}
-                        >
-                            DigiWiz
-                        </Typography>
-                        <Search>
-                            <SearchIconWrapper>
-                                <SearchIcon/>
-                            </SearchIconWrapper>
-                            <StyledInputBase
-                                placeholder="Search…"
-                                inputProps={{'aria-label': 'search'}}
-                            />
-                        </Search>
-                    </Toolbar>
-                </AppBar>
-            </Box>
+            <AppBar/>
             <Box>
                 <Toolbar/>
                 <Card>
                     <CardContent>
-                        <Select
-                            value={font}
-                            onChange={(e) => setFont(e.target.value)}
-                        >
-                            <MenuItem value={'Arial'}>Arial</MenuItem>
-                            <MenuItem value={'Helvetica'}>Helvetica</MenuItem>
-                            <MenuItem value={'OpenDyslexic'}>OpenDyslexic</MenuItem>
-                        </Select>
-                        <Slider
-                            min={16}
-                            max={48}
-                            value={fontSize}
-                            onChange={(_, v) => setFontSize(Array.isArray(v) ? v[0] : v)}
-                        />
+                        <FontSwitcher/>
                     </CardContent>
                     <CardContent>
                         Lorem Ipsum dolor sit amet.
