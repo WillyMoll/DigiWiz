647dc9b80e0b2d3d70ca707315f27dd3c7438d0c
Willy <cromir@mail.ch>
2021-10-23 02:48:02 +0200

    Add deploy script

--- frontend/src/components/pages/UseCasePage.tsx
+++ frontend/src/components/pages/UseCasePage.tsx
@@ -1,8 +1,16 @@
-import {Button, Paper} from "@mui/material";
 import {Solution} from "../Solution";
+import {Button, Paper, Typography} from "@mui/material";
+import {useParams} from "react-router-dom";
 
 export const UseCasePage = () => {
+    const {usecase} = useParams<{ usecase: string }>()
+
     return <Paper>
+        <Typography
+            component={"h3"}
+        >
+            {usecase}
+        </Typography>
         {/*TODO: top 3*/}
         <Button>Alle Anzeigen</Button>
         <Solution
--- frontend/src/service/ApiService.ts
+++ frontend/src/service/ApiService.ts
@@ -1,5 +1,6 @@
 import {api} from "../config/api";
 
 export const ApiService = {
-    getQuestion: () => api.get('/questions').then(r => r.data) //TODO: type
+    getQuestions: () => api.get('/questions').then(r =>r.data),
+    getQuestion: () => api.get('/questions').then(r => (r.data as any[])[Math.floor(Math.random() / (r.data as any[]).length)]) //TODO: type
 }
