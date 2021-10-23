02101261cdb5617f1f594d6ac1ebc22e46b1412a
Lukas Wipf <Lukas.Wipf@gmx.ch>
2021-10-23 15:34:49 +0200

    Fix solutions for smartphone

--- frontend/src/components/Solution.tsx
+++ frontend/src/components/Solution.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Button, Card, CardActions, CardContent, CardMedia, Typography} from "@mui/material";
+import {Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography} from "@mui/material";
 
 interface SolutionProps {
     title: string,
@@ -8,24 +8,30 @@ interface SolutionProps {
     webUrl: string,
 }
 
-export const Solution = (props:SolutionProps) => {
-    return <Card sx={{ margin: '20px', display:'flex' }}>
-        <Box sx={{  width: '80%' }}>
+export const Solution = (props: SolutionProps) => {
+    return <Card sx={{margin: '20px', display: 'flex', flexDirection: {xs: 'column', sm: 'row'}}}>
+        <Box style={{flexGrow: 1}}>
+            <CardHeader title={props.title}/>
+            <CardMedia
+                component="img"
+                sx={{display: {xs: "block", sm: 'none'}}}
+                image={props.imgUrl}
+            />
             <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {props.title}
-                </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {props.text}
                 </Typography>
                 <CardActions>
-                    <Button sx={{paddingLeft: '30px', paddingRight: '30px' }} variant="contained" size="small" onClick={() => {window.open(props.webUrl)}}>Webseite</Button>
+                    <Button sx={{paddingLeft: '30px', paddingRight: '30px'}} variant="contained" size="small"
+                            onClick={() => {
+                                window.open(props.webUrl)
+                            }}>Webseite</Button>
                 </CardActions>
             </CardContent>
         </Box>
         <CardMedia
             component="img"
-            sx={{ width: '20%' }}
+            sx={{display: {xs: "none", sm: 'block'}, float: "right", width: "20%"}}
             image={props.imgUrl}
         />
     </Card>
