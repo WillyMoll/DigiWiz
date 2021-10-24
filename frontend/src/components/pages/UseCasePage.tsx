8c936f065fed4133e19c2658db99884b56c8ee75
Niklas van der Heide <vandenik@students.zhaw.ch>
2021-10-24 11:14:29 +0200

    pixil no solutions

--- frontend/src/components/pages/UseCasePage.tsx
+++ frontend/src/components/pages/UseCasePage.tsx
@@ -51,7 +51,7 @@ export const UseCasePage = () => {
                             <Box sx={{height: 10}}/>
                             <Typography variant="body2" color="text.secondary">
                                 Hier sind lösungen die ich aus ihren antworten zusammengezaubert habe.
-                                Dabe finden sie für jede Lösung eine kurze Beschreibung sowie inenen Link auf die Webseite des jeweiligen Anbieter.
+                                Dabei finden sie für jede Lösung eine kurze Beschreibung sowie ihnen Link auf die Webseite des jeweiligen Anbieter.
                             </Typography>
                             <Box sx={{height: 10}}/>
                             <Typography variant="body2" color="text.secondary">
@@ -60,9 +60,29 @@ export const UseCasePage = () => {
                         </CardContent>
                     </Card>
                 </Grid>
-                {data.map(u => <Grid item xs={12} key={u.id}><UseCase {...u}/></Grid>)}</Grid> : <Card>
-                <CardContent>So wie es aussieht sind Sie schon perfekt digitalisiert.</CardContent>
-            </Card>) :
+                {data.map(u => <Grid item xs={12} key={u.id}><UseCase {...u}/></Grid>)}</Grid> : <Grid
+                item xs={12}>
+                <Card sx={{
+                    display: 'flex',
+                    flexDirection: {xs: 'column', sm: 'row'},
+                    justifyContent: 'space-between'
+                }}>
+                    <CardMedia
+                        component="img"
+                        image={img}
+                        sx={{width: 300, objectFit: 'contain', display: {xs: 'block', sm: 'block'}}}
+                    />
+                    <CardContent>
+                        <Typography variant="h6" component="div">
+                            Huch!
+                        </Typography>
+                        <Box sx={{height: 10}}/>
+                        <Typography variant="body2" color="text.secondary">
+                            Sieht aus als wären sie schon digitalisiert. Ich kannn da leider nichts mehr anbieten.
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>) :
             <Card>
                 <CardContent>Laden...</CardContent>
             </Card>}
