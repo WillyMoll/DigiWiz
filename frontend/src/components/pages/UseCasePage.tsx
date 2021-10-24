2e69be8144bf66b0dc3317af4592c1e4f5d4c099
Niklas van der Heide <vandenik@students.zhaw.ch>
2021-10-24 11:07:39 +0200

    typo

--- frontend/src/components/pages/UseCasePage.tsx
+++ frontend/src/components/pages/UseCasePage.tsx
@@ -51,7 +51,7 @@ export const UseCasePage = () => {
                             <Box sx={{height: 10}}/>
                             <Typography variant="body2" color="text.secondary">
                                 Hier sind lösungen die ich aus ihren antworten zusammengezaubert habe.
-                                Dabe finden sie für jede Lösung eine kurze VBeschreibung sowie inenen Link auf die Webseite des jeweiligen Anbieter.
+                                Dabe finden sie für jede Lösung eine kurze Beschreibung sowie inenen Link auf die Webseite des jeweiligen Anbieter.
                             </Typography>
                             <Box sx={{height: 10}}/>
                             <Typography variant="body2" color="text.secondary">
